5cf21de68f739954f36ea4b93a60ab7bdf1216a1
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-10-07 16:07:37 -0400

    Initial commit

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use('/static', express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Home Page
+router.get('/', (req, res) => {
+  res.render('home', { title: 'Home Page' }); // Render the 'home.ejs' template
+});
+
+// About Page
+router.get('/about', (req, res) => {
+  res.render('about', { title: 'About Me' }); // Render the 'about.ejs' template
+});
+
+// Projects Page
+router.get('/projects', (req, res) => {
+  res.render('projects', { title: 'Projects' }); // Render the 'projects.ejs' template
+});
+
+// Services Page
+router.get('/services', (req, res) => {
+  res.render('services', { title: 'Services' }); // Render the 'services.ejs' template
+});
+
+// Contact Page
+router.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Contact Me' }); // Render the 'contact.ejs' template
 });
 
 module.exports = router;
